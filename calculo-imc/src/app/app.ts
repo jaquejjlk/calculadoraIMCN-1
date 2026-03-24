eafcd89c59653e68bf07a1c8c8d49b435fc0e5ee
Jaqueline <jaqueline_fanton@estudante.sesisenai.org.br>
2026-03-23 21:44:45 -0300

    JQ

--- calculo-imc/src/app/app.ts
+++ calculo-imc/src/app/app.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('calculo-imc');
+  //Atributos
+  peso: number = 0;
+  altura: number = 0;
+  imc: number = 0;
+  resultado: String = '';
+  //Método
+  calcularIMC() {
+    this.imc = this.peso / (this.altura * this.altura);
+    if (this.imc < 18.5) {
+      this.resultado = "Baixo peso.";
+    } else if (this.imc <= 25) {
+      this.resultado = "Normal";
+    }
+    else if (this.imc <= 30) {
+      this.resultado = "Sobrepeso";
+    } else {
+      this.resultado = "Obesidade";
+    }
+  }
+
 }
